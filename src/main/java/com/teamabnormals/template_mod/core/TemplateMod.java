457ba5075d99febf19347ac7ec19a19b9da31a4b
baguchan <82218336+baguchan@users.noreply.github.com>
2021-06-20 11:49:40 +0900

    Initial commit

---
+++ src/main/java/com/teamabnormals/template_mod/core/TemplateMod.java
@@ -0,0 +1,43 @@
+package com.teamabnormals.template_mod.core;
+
+import com.minecraftabnormals.abnormals_core.core.util.registry.RegistryHelper;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(TemplateMod.MOD_ID)
+public class TemplateMod {
+	public static final String MOD_ID = "template_mod";
+	public static final RegistryHelper REGISTRY_HELPER = new RegistryHelper(MOD_ID);
+
+	public TemplateMod() {
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+		REGISTRY_HELPER.register(bus);
+		MinecraftForge.EVENT_BUS.register(this);
+
+		bus.addListener(this::commonSetup);
+		bus.addListener(this::clientSetup);
+		bus.addListener(this::dataSetup);
+	}
+
+	private void commonSetup(FMLCommonSetupEvent event) {
+		event.enqueueWork(() -> {
+
+		});
+	}
+
+	private void clientSetup(FMLClientSetupEvent event) {
+		event.enqueueWork(() -> {
+
+		});
+	}
+
+	private void dataSetup(GatherDataEvent event) {
+
+	}
+}
---
+++ src/main/java/com/teamabnormals/template_mod/core/other/TemplateEvents.java
@@ -0,0 +1,9 @@
+package com.teamabnormals.template_mod.core.other;
+
+import com.teamabnormals.template_mod.core.TemplateMod;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = TemplateMod.MOD_ID)
+public class TemplateEvents {
+
+}
